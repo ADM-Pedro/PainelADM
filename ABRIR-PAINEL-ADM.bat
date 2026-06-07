972f8136d07d3e20f6fe3bb571eeb278cbb239f8
ADM-Pedro <joojmania@gmail.com>
2026-06-07 12:33:21 -0300

    Add files via upload

---
+++ ABRIR-PAINEL-ADM.bat
@@ -0,0 +1,8 @@
+@echo off
+REM ============================================================
+REM  Painel do ADM - DK Diario Brawl
+REM  Uso interno dos 4 ADMs. Duplo clique para abrir.
+REM ============================================================
+title Painel ADM - DK Diario Brawl
+start "" "%~dp0painel-adm.html"
+exit
